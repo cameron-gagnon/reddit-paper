e35cc240038cdcac940c311c714bd8846d2475fc
Cameron Gagnon <cgagnon@umich.edu>
2015-03-17 00:29:49 -0400

    added technologyporn and imaginarystarscapes to default subreddits

--- reddit-paper.py
+++ reddit-paper.py
@@ -45,7 +45,7 @@ from urllib.error import HTTPError,URLError
 
 #sets up global vars
 CREDENTIALS = "user_pass.txt"
-SUBREDDITS = "spaceporn+earthporn+waterporn"
+SUBREDDITS = "spaceporn+earthporn+waterporn+technologyporn+imaginarystarscapes"
 USERAGENT = "Reddit wallpaper changer script /u/countfapula69 " \
 	    "beta testing"
 SETWALLPAPER = "gsettings set org.gnome.desktop.background " \
