4849c5dcf7b041802487c0e64a4d02b69fc13de9
Cameron Gagnon <cgagnon@umich.edu>
2015-06-28 02:09:15 -0400

    tweaked icon more

--- setup.py
+++ setup.py
@@ -1,16 +1,15 @@
 import sys
 from cx_Freeze import setup, Executable
 
-executables = [Executable("gui.pyw", base = ("Win32GUI" if sys.platform == 'win32' else None), icon = "./images/reddit.ico", targetName = "Reddit Paper.exe")]
-
+executables = [Executable("gui.pyw", base = ("Win32GUI" if sys.platform == 'win32' else None), icon = "./images/redditicon_48.ico", targetName = "Reddit Paper.exe")]
 included_files = ["./images", "redditpaper.pyw", "detools",
-				  "C:\\Python34\\Lib\\site-packages\\praw\\praw.ini"]
+		  "C:\\Python34\\Lib\\site-packages\\praw\\praw.ini"]
 
 install_requires = ["praw", "urllib.request", "bs4", "PIL", "sqlite3", "tkinter"]
 
 setup(
         name = "Reddit Paper",
-        version = "1.0.1.0",
+        version = "1.0",
         author = "Cameron Gagnon",
         author_email = 'cameron.gagnon@gmail.com',
         url = "http://github.com/cameron-gagnon/reddit-paper",
