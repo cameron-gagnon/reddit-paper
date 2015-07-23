a79fc35a95655041e912edb160d0df853af8463f
Cameron Gagnon <cgagnon@umich.edu>
2015-07-23 00:11:07 -0400

    tweaked formatting

--- setup.py
+++ setup.py
@@ -6,17 +6,18 @@ executables = [Executable("gui.py",
                           targetName = "Reddit Paper.exe",
                           shortcutName = "Reddit Paper",
                           shortcutDir = "DesktopFolder")] #base = ("Win32GUI" if sys.platform == 'win32' else None),]
-included_files = ["./images", "redditpaper.py", #"detools",
+included_files = ["./images", "redditpaper.py",
 		          "C:\\Python34\\Lib\\site-packages\\praw\\praw.ini",
                   "cacert.pem",
+                  # for the call to redditpaper with subprocess
                   "C:\\Python34\\pythonw.exe"]
 
 install_requires = ["praw", "urllib.request", "bs4", "PIL", "sqlite3", "tkinter"]
 
 build_exe_options = {
-                        "include_files": included_files,
-                        "packages": install_requires,
-                        "include_msvcr": True
+        "include_files": included_files,
+        "packages": install_requires,
+        "include_msvcr": True
 }
 
 shortcut_table = [
