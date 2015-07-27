61e26a1c1e7e08203e8995a4d6ac518791f9f6ae
Cameron Gagnon <cgagnon@umich.edu>
2015-07-27 11:08:26 -0400

    updated .py's to .pyw's

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 import sys
 from cx_Freeze import setup, Executable
 
-executables = [Executable("gui.py",  
+executables = [Executable("gui.pyw",  
                           icon = "./images/rp_sq_48.ico",
                           targetName = "Reddit Paper.exe",
                           shortcutName = "Reddit Paper",
                           shortcutDir = "DesktopFolder")] #base = ("Win32GUI" if sys.platform == 'win32' else None),]
-included_files = ["./images", "redditpaper.py",
+included_files = ["./images", "redditpaper.pyw",
 		          "C:\\Python34\\Lib\\site-packages\\praw\\praw.ini",
                   "cacert.pem",
                   # for the call to redditpaper with subprocess
